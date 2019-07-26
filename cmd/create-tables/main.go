abf593bef859a28b365e923bb6f6f1c7ba203e8c
Jaydeep Solanki <jaydp17@gmail.com>
2019-07-26 23:33:10 +0530

    update cmd/create-tables to create movies & cinemas tables

--- cmd/create-tables/main.go
+++ cmd/create-tables/main.go
@@ -6,16 +6,83 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/awserr"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/jaydp17/movie-ticket-watcher/pkg/cinemas"
 	"github.com/jaydp17/movie-ticket-watcher/pkg/cities"
 	"github.com/jaydp17/movie-ticket-watcher/pkg/db"
+	"github.com/jaydp17/movie-ticket-watcher/pkg/movies"
 )
 
 func main() {
 	createCitiesTable()
+	createMoviesTable()
+	createCinemasTable()
 }
 
 func createCitiesTable() {
 	tableName := cities.TableName
+	input := &dynamodb.CreateTableInput{
+		TableName: aws.String(tableName),
+		AttributeDefinitions: []dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("ID"),
+				AttributeType: dynamodb.ScalarAttributeTypeS,
+			},
+		},
+		KeySchema: []dynamodb.KeySchemaElement{
+			{
+
+				AttributeName: aws.String("ID"),
+				KeyType:       dynamodb.KeyTypeHash,
+			},
+		},
+		BillingMode: dynamodb.BillingModePayPerRequest,
+	}
+	req := db.Client.CreateTableRequest(input)
+	sendReq(&req)
+}
+
+func createMoviesTable() {
+	tableName := movies.TableName
+	input := &dynamodb.CreateTableInput{
+		TableName: aws.String(tableName),
+		AttributeDefinitions: []dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("ID"),
+				AttributeType: dynamodb.ScalarAttributeTypeS,
+			},
+		},
+		KeySchema: []dynamodb.KeySchemaElement{
+			{
+
+				AttributeName: aws.String("ID"),
+				KeyType:       dynamodb.KeyTypeHash,
+			},
+		},
+		BillingMode: dynamodb.BillingModePayPerRequest,
+	}
+
+	// CityID Global Secondary Index
+	//cityIndex := dynamodb.GlobalSecondaryIndex{
+	//	IndexName: aws.String("city-id-index"),
+	//	KeySchema: []dynamodb.KeySchemaElement{
+	//		{
+	//
+	//			AttributeName: aws.String("cityID"),
+	//			KeyType:       dynamodb.KeyTypeHash,
+	//		},
+	//	},
+	//	Projection: &dynamodb.Projection{
+	//		ProjectionType: dynamodb.ProjectionTypeAll,
+	//	},
+	//}
+	//input.GlobalSecondaryIndexes = append(input.GlobalSecondaryIndexes, cityIndex)
+
+	req := db.Client.CreateTableRequest(input)
+	sendReq(&req)
+}
+
+func createCinemasTable() {
+	tableName := cinemas.TableName
 	input := &dynamodb.CreateTableInput{
 		TableName: aws.String(tableName),
 		AttributeDefinitions: []dynamodb.AttributeDefinition{
